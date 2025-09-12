e9994a20e523ccff8b60f6e9fd9a28e1c5a6fddb
Kittipak <AliveKittipak@gmail.com>
2025-09-12 22:13:01 +0700

    chore: init

---
+++ ems-backend/src/main/java/net/javaguides/ems_backend/service/implementation/EmployeeServiceImplementation.java
@@ -0,0 +1,73 @@
+package net.javaguides.ems_backend.service.implementation;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems_backend.dto.EmployeeDto;
+import net.javaguides.ems_backend.entity.Employee;
+import net.javaguides.ems_backend.exception.ResourceNotFoundException;
+import net.javaguides.ems_backend.mapper.EmployeeMapper;
+import net.javaguides.ems_backend.repository.EmployeeRepository;
+import net.javaguides.ems_backend.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImplementation implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee is not exist with the given id :" + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) {
+
+        // Find employee by id from the database
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+                () -> new ResourceNotFoundException("Employee is not exist with the given id:" + employeeId));
+
+        // Update employee details
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+
+        // Save updated employee to the database
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        // Check whether employee exist in the database or not
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee is not exist with the given id :" + employeeId));
+
+        // Delete employee from the database
+        employeeRepository.deleteById(employeeId);
+    }
+}
